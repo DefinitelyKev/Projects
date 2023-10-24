aba1c10419a9c7bcb88da9d8092085919015f121
Kevin Nguyen <Kevinnk259@gmail.com>
2023-10-24 19:32:20 +1100

    ready for portfolio deployment test

--- portfolio_website/project/portfolio/models.py
+++ portfolio_website/project/portfolio/models.py
@@ -4,9 +4,12 @@ from datetime import datetime
 # Create your models here.
 
 
-class Projects(models.Model):
+class Project(models.Model):
     title = models.CharField(max_length=300)
-    image = models.ImageField(upload_to="images/")
+    image = models.ImageField(upload_to="images/", default=None)
     description = models.TextField(blank=True)
     technology = models.CharField(max_length=30)
     date_pub = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        return self.title
