78b64302cbdc8a5079ae1309b6432016b966aaff
Kevin Nguyen <Kevinnk259@gmail.com>
2023-10-25 13:32:59 +1100

    trying to deploy on railway

--- portfolio_website/project/project/settings.py
+++ portfolio_website/project/project/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-CSRF_TRUSTED_ORIGINS = ["https://portfolio-website.fly.dev/", "http://127.0.0.1:8000/"]
+CSRF_TRUSTED_ORIGINS = ["https://portfolio-website.fly.dev/"]
 
 CSRF_COOKIE_SECURE = False
 
@@ -124,6 +124,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
