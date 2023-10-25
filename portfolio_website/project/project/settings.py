802c63f308e55f7480f226344f6b532df7c339a8
Kevin Nguyen <Kevinnk259@gmail.com>
2023-10-25 13:41:42 +1100

    update static

--- portfolio_website/project/project/settings.py
+++ portfolio_website/project/project/settings.py
@@ -123,7 +123,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
