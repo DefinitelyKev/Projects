89697542d81acad7c33b1d8ab3488ae4a1fbb3e4
Kevin Nguyen <Kevinnk259@gmail.com>
2023-11-18 13:19:32 +1100

    updated project names to include main technology used. deleted old projects and deprecated current

--- email_sender/newsletter/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from django.forms import ModelForm
-from .models import SubscribedUsers
-
-
-class SubscribedUsersForm(ModelForm):
-    class Meta:
-        model = SubscribedUsers
-        fields = "__all__"
